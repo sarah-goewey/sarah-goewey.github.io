0239d34fd4c62589ebeb0972e0a739f01674ddab
Sarah Goewey <sarah.goewey@yahoo.com>
2023-09-11 18:50:02 -0400

    movecd html file

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/dist", express.static(path.join(__dirname, "../dist")));
 app.use("/static", express.static(path.join(__dirname, "../static")));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../static/index.html"))
-);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
