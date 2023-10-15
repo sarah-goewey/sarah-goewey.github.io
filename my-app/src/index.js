3eb93a9b259a98938f7a35802527f880c24db2d7
Sarah Goewey <sarah.goewey@yahoo.com>
2023-10-15 17:10:40 -0400

    significant rework to add sass

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+//import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/custom_bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
